3e5cc0e016ad74323a4fd8e12fb6a351e610c6ee
H0bb1tM1ndTr1x <H0bb1tM1ndTr1x@gmail.com>
2021-11-10 16:49:11 -0500

    v0.11.10.21

--- Factions/Tau Empire/Units.py
+++ Factions/Tau Empire/Units.py
@@ -1,25 +1,396 @@
 operatives = [
-    {
-        "name":"",
+    {   # Fire Warrior Shas'la
+        "opname":"Fire Warrior Shas'la",
         "stats":{
-            "move":"",
-            "apl":"",
-            "gpact":"",
-            "def":"",
-            "save":"",
-            "wounds":"",
+            "move":"6\"",
+            "apl":"2",
+            "gpact":"1",
+            "def":"3",
+            "save":"4+",
+            "wounds":"7",
         },
         "rweaps":[
+            {   # Pulse Blaster
+                "wname":"Pulse Blaster",
+                "profiles":[
+                    {
+                        "pname":"Close Range",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"4/5",
+                        "srule":[
+                            "Range 6\""
+                            "Armour Penetration 1"
+                        ],
+                        "crits":[
+                        ],
+                    },
+                    {
+                        "pname":"Long Range",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"3/4",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+            {   # Pulse Carbine
+                "wname":"Pulse Carbine",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+            {   # Pulse Rifle
+                "wname":"Pulse Rifle",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
         ],
         "mweaps":[
+            {   # Gun Butt
+                "wname":"Gun Butt",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"3",
+                        "wskill":"5+",
+                        "dmg":"2/3",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
         ],
         "abs":[
         ],
         "uacts":[
         ],
-        "fackey":"",
-        "faction":"",
+        "fackey":"Hunter Cadre",
+        "faction":"T'au",
         "keywords":[
+            "Fire Warrior"
+            "Shas'la"
+        ],
+    },
+    {   # Fire Warrior Shas'ui
+        "opname":"Fire Warrior Shas'ui",
+        "stats":{
+            "move":"6\"",
+            "apl":"2",
+            "gpact":"1",
+            "def":"3",
+            "save":"4+",
+            "wounds":"8",
+        },
+        "rweaps":[
+            {   # Pulse Blaster
+                "wname":"Pulse Blaster",
+                "profiles":[
+                    {
+                        "pname":"Close Range",
+                        "atts":"4",
+                        "wskill":"3+",
+                        "dmg":"4/5",
+                        "srule":[
+                            "Range 6\""
+                            "Armour Penetration 1"
+                        ],
+                        "crits":[
+                        ],
+                    },
+                    {
+                        "pname":"Long Range",
+                        "atts":"4",
+                        "wskill":"3+",
+                        "dmg":"3/4",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+            {   # Pulse Carbine
+                "wname":"Pulse Carbine",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"3+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+            {   # Pulse Rifle
+                "wname":"Pulse Rifle",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"3+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        ],
+        "mweaps":[
+            {   # Gun Butt
+                "wname":"Gun Butt",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"3",
+                        "wskill":"5+",
+                        "dmg":"2/3",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        ],
+        "abs":[
+        ],
+        "uacts":[
+        ],
+        "fackey":"Hunter Cadre",
+        "faction":"T'au",
+        "keywords":[
+            "Leader"
+            "Fire Warrior"
+            "Shas'ui"
+        ],
+    },
+    {   # Pathfinder Shas'la
+        "opname":"Pathfinder Shas'la",
+        "stats":{
+            "move":"6\"",
+            "apl":"2",
+            "gpact":"1",
+            "def":"3",
+            "save":"5+",
+            "wounds":"7",
+        },
+        "rweaps":[
+            {   # Pulse Carbine
+                "wname":"Pulse Carbine",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        ],
+        "mweaps":{
+            {   # Gun Butt
+                "wname":"Gun Butt",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"3",
+                        "wskill":"5+",
+                        "dmg":"2/3",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        },
+        "abs":[
+        ],
+        "uacts":[
+            "Markerlight"
+        ],
+        "fackey":"Hunter Cadre",
+        "faction":"T'au",
+        "keywords":[
+            "Pathfinder"
+            "Shas'la"
+        ],
+    },
+    {   # Pathfinder Heavy Gunner
+        "opname":"Pathfinder Heavy Gunner",
+        "stats":{
+            "move":"6\"",
+            "apl":"2",
+            "gpact":"1",
+            "def":"3",
+            "save":"5+",
+            "wounds":"7",
+        },
+        "rweaps":[
+            {   # Ion Rifle
+                "wname":"Ion Rifle",
+                "profiles":[
+                    {
+                        "pname":"Standard",
+                        "atts":"5",
+                        "wskill":"4+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                            "Piercing 1"
+                        ],
+                    },
+                    {
+                        "pname":"Overcharge",
+                        "atts":"5",
+                        "wskill":"4+",
+                        "dmg":"5/6",
+                        "srule":[
+                            "Armour Penetration 1"
+                            "Hot"
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+            {   # Rail Rifle
+                "wname":"Rail Rifle",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"4+",
+                        "dmg":"4/4",
+                        "srule":[
+                            "Armour Penetration 1"
+                            "Lethal 5+"
+                        ],
+                        "crits":[
+                            "Mortal Wounds 2"
+                        ],
+                    },
+                ]
+            },
+        ],
+        "mweaps":{
+            {   # Gun Butt
+                "wname":"Gun Butt",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"3",
+                        "wskill":"5+",
+                        "dmg":"2/3",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        },
+        "abs":[
+        ],
+        "uacts":[
+        ],
+        "fackey":"Hunter Cadre",
+        "faction":"T'au",
+        "keywords":[
+            "Pathfinder"
+            "Heavy Gunner"
+        ],
+    },
+    {   # Pathfinder Shas'ui
+        "opname":"Pathfinder Shas'ui",
+        "stats":{
+            "move":"6\"",
+            "apl":"2",
+            "gpact":"1",
+            "def":"3",
+            "save":"5+",
+            "wounds":"8",
+        },
+        "rweaps":[
+            {   # Pulse Carbine
+                "wname":"Pulse Carbine",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"4",
+                        "wskill":"3+",
+                        "dmg":"4/5",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        ],
+        "mweaps":{
+            {   # Gun Butt
+                "wname":"Gun Butt",
+                "profiles":[
+                    {
+                        "pname":"",
+                        "atts":"3",
+                        "wskill":"5+",
+                        "dmg":"2/3",
+                        "srule":[
+                        ],
+                        "crits":[
+                        ],
+                    },
+                ]
+            },
+        },
+        "abs":[
+        ],
+        "uacts":[
+            "Markerlight"
+        ],
+        "fackey":"Hunter Cadre",
+        "faction":"T'au",
+        "keywords":[
+            "Leader"
+            "Pathfinder"
+            "Shas'la"
         ],
     },
 ]
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # WH40K_KillTeam_v2
 
+## v0.11.10.21
+
+## Templates
+
+- Weapon template created
+- Unit template created
+  - Templates merged
+
+### Factions
+
+- Tau Empire
+  - Unit data updated
+
+--------------------------------------------------------------------
+
 ## v0.11.09.21
 
 ### Project Notes
